97c3467c797f22816b4a14e62dad85a4039729a6
Tuong Duy Tuyen <tuyentd.ba12-189@st.usth.edu.vn>
2024-10-14 20:04:21 +0700

    back index

--- CTF_playground/src/index.php
+++ CTF_playground/src/index.php
@@ -4,61 +4,12 @@ $username = "user";
 $password = "userpassword";
 $dbname = "ctf_db";
 
-// Create database connection
-$conn = new mysqli($servername, $username, $password, $dbname);
 
-// Check connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+#common format to connect to db in docker
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
+header("Location: Login_or_register.html");
+exit();
 ?>
-
-<?php
-header('Content-Type: application/json');
-
-// Get the request method and path
-$request_method = $_SERVER['REQUEST_METHOD'];
-$path_info = $_SERVER['REQUEST_URI'] ?? '/';
-
-// Remove query string from request URI
-$path_info = strtok($path_info, '?');
-
-// Route requests to the appropriate handler
-switch ($path_info) {
-    case '/api/users/register':
-        if ($request_method === 'POST') {
-            require 'users.php';
-            registerUser();
-        } else {
-            sendError(405, 'Method Not Allowed');
-        }
-        break;
-
-    case '/api/users/login':
-        if ($request_method === 'POST') {
-            require 'users.php';
-            loginUser();
-        } else {
-            sendError(405, 'Method Not Allowed');
-        }
-        break;
-
-    case '/api/challenges/submit':
-        if ($request_method === 'POST') {
-            require 'challenges.php';
-            submitFlag();
-        } else {
-            sendError(405, 'Method Not Allowed');
-        }
-        break;
-
-    default:
-        sendError(404, 'Not Found');
-}
-
-// Function to send error responses
-function sendError($code, $message) {
-    http_response_code($code);
-    echo json_encode(['error' => $message]);
-    exit;
-}
